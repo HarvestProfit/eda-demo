c7b46a2214d229907cb0e438bdfac3fefb0c591f
Jaryd Krishnan <jaryd@hey.com>
2021-03-11 14:29:37 -0600

    Add some tests to the home page

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('<App />', () => {
-  it('should render a <HashRouter />', () => {
+  it('should render a <BrowserRouter />', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(BrowserRouter).length).toEqual(1);
   });
